aedda95a805d54f7deac7f48a50743f663e8ebd5
Guillaume Renard <renardguillaume@gmail.com>
2018-07-31 14:18:13 +0200

    add content type

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const app = express()
 const port = process.env.NODE_PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send(JSON.stringify(req.headers, null, 4))
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(req.headers, null, 4));
 })
 
 app.listen(port, function () {
-  console.log('HeadersReturn app listening on port ' + port)
+  console.log('HeadersReturn app listening on port ' + port);
 })
